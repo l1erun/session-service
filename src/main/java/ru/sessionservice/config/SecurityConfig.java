5499baffbc82a36ad0cd9c6342b96cc6470f707d
Алексей Степанов <aleksejstepanov@MacBook-Air-Aleksej.local>
2024-10-10 10:09:24 +0300

    Add Swagger

---
+++ src/main/java/ru/sessionservice/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package ru.sessionservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable());  // Отключаем CSRF, если не нужен
+        return http.build();
+    }
+}
